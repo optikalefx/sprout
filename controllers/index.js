d36a5ca59f84110caa5754f123ef3dbe63cac9f5
Sean Clark <sean@connectai.com>
2014-12-12 20:43:54 -0500

    cleaned up init files added mongo connection MongoDB and Lodash

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,37 @@
+/* globals app */
+
+var MongoClient = require('mongodb').MongoClient;
+
+app.run = function(generatorFunction) {
+    var generatorItr = generatorFunction(function(one, two) {
+		if(arguments.length > 1 && one === null) {
+			return resume(two);
+		} else {
+			return resume(one);
+		}
+	});
+    function resume(callbackValue) {
+        generatorItr.next(callbackValue);
+    }
+    generatorItr.next();
+};
+
+
+
 var routes = {
+
+	"*"(req,res,next) {
+		app.run(function* (resume) {
+			// connect to mongo
+			if(!app.db || !app.db.collection) {
+				app.db = yield MongoClient.connect(app.config.db.host,resume);
+				next();
+			} else {
+				next();
+			}
+		});
+	},
+
 	"/"(req, res, next) {
 		// Render the view found at /views/index.html.
 		res.view("index");
