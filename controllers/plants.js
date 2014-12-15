870191986c8718b34bfc749c182b49d4997018fb
Sean Clark <sean@connectai.com>
2014-12-15 09:33:10 -0500

    can now find plants based on given month and state

---
+++ controllers/plants.js
@@ -0,0 +1,41 @@
+
+module.exports = {
+
+	"index"(req, res, next) {
+		res.view();
+	},
+
+	"search"(req,res) {
+
+		var state = req.body.state;
+
+		app.run(function* (resume) {
+
+			// get current month
+			var d = new Date();
+			var month = d.getMonth() + 1;
+
+			var climate = yield app.db.collection("climate").findOne({
+				state: state
+			}, resume);
+
+			var zones = climate.zones.map(function(zone) {
+				var obj = {};
+				obj["zones." + zone] = {$in: [month]};
+				return obj;
+			});
+
+			console.log(zones);
+
+			// get all plants for these climates
+			var plants = yield app.db.collection("plant").find({
+			    $or : zones
+			}).toArray(resume);
+
+			res.json(plants);
+
+		});
+
+	}
+
+};
