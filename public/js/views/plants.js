93826359f75e2281f082f00667348734d7f755ac
Sean Clark <sean@connectai.com>
2015-01-11 11:48:47 -0500

    added arrows and organized plant.js

--- public/js/views/plants.js
+++ public/js/views/plants.js
@@ -1,57 +1,76 @@
 /* globals navigator, $, Handlebars, _ */
+(function() {
 
-// html5 location
-navigator.geolocation.getCurrentPosition(function(location) {
-	var lat = location.coords.latitude;
-	var lon = location.coords.longitude;
+	var app = {
 
-    // update season icon
-    var seasons = [
-        {season:"winter", months: [12,1,2]},
-        {season:"spring", months: [3,4,5]},
-        {season:"summer", months: [6,7,8]},
-        {season:"fall", months: [9,10,11]}
-    ];
-    var d = new Date();
-    var currentMonth = d.getMonth() + 1;
-    var season =  _.find(seasons, function(obj) {
-        return _.contains(obj.months, currentMonth);
-    }).season;
+		init: function() {
 
-    // swap out season image now
-    $(".season img").attr("src", "/img/" + season + ".png");
+			this.changeSeason();
+			this.setLocation();
 
+		},
 
-	$.ajax({ url:'http://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lon+'&sensor=true',
-         success: function(data){
+		changeSeason: function() {
+			// map of all seasons to months. This hemisphere only
+			var seasons = {
+				winter: [12,1,2],
+				spring: [3,4,5],
+				summer: [6,7,8],
+				fall: [9,10,11]
+			};
 
-            // find state
-            var addr = _.find(data.results[0].address_components, function(res) {
-                return res.types && res.types[0] === "administrative_area_level_1";
-            });
+			// figure out what season we are in
+			var currentMonth = (new Date()).getMonth() + 1;
+			var season =  _.findKey(seasons, function(months) {
+				return _.contains(months, currentMonth);
+			});
 
-             var state = addr.long_name;
-             state = state.toLowerCase();
-             $(".flagInner").css("backgroundImage","url(/img/flags/"+state+"-flag.jpg)");
+			// swap out season image now
+			$(".season img").attr("src", "/img/" + season + ".png");
+		},
 
-            var stateCode = addr.short_name;
+		setLocation: function() {
+			navigator.geolocation.getCurrentPosition(function(location) {
+				var lat = location.coords.latitude;
+				var lon = location.coords.longitude;
 
-             // call to server with our maryland data
-            $.post("/plants/search",{state: stateCode}).then(function(plants) {
+				$.ajax({ url:'http://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lon+'&sensor=true',
+					 success: function(data) {
+					 	// find state
+						var addr = _.find(data.results[0].address_components, function(res) {
+							return res.types && res.types[0] === "administrative_area_level_1";
+						});
 
-                plants = _.shuffle(plants);
-                plants = plants.splice(0,4);
+						 var state = addr.long_name;
+						 state = state.toLowerCase();
+						 $(".flagInner").css("backgroundImage","url(/img/flags/"+state+"-flag.jpg)");
 
-                // get the plants template
-                var template = Handlebars.compile($("#plantsTemplate").html());
-                template = template({plants:plants});
-                $(".plantList").html(template);
+						var stateCode = addr.short_name;
 
-            });
+						app.getPlants(stateCode);
+					 }
+				});
+			});
+		},
 
-         }
-    });
+		getPlants: function(stateCode) {
+			 // call to server with our Maryland data
+			$.post("/plants/search",{state: stateCode}).then(function(plants) {
 
+				plants = _.shuffle(plants);
+				plants = plants.splice(0,4);
 
+				// get the plants template
+				var template = Handlebars.compile($("#plantsTemplate").html());
+				template = template({plants:plants});
+				$(".plantList").html(template);
 
-});
+			});
+		}
+
+	};
+
+	// go!
+	app.init();
+
+})();
